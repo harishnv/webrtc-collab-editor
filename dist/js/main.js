ff0b5fe1287e26dee3ccd9bac5bcab8ca8698ff4
harishnv <hu72005@gmail.com>
2018-09-10 05:36:03 +0530

    display user input with user name in list group added

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
-var mod_oper_running=false;
-var connected_to_io=false;
-var username;
+  var mod_oper_running=false;
+  var connected_to_io=false;
+  var username;
   $('#button_connect').click(function(event) {
     /* Act on the event */
     if(!connected_to_io){
-    username=$('#txt_uname').val();
-    socket.emit('user_join', username);
-    console.log("button clicked "+username);
-    $('#connect_modal').modal("hide");
+      username=$('#txt_uname').val();
+      socket.emit('user_join', username);
+      console.log("button clicked "+username);
+      $('#connect_modal').modal("hide");
     }
   });
 
@@ -20,70 +20,72 @@ var username;
     mode: 'application/x-httpd-php'
   });
   editor.on("change", function(instance,obj){
+
     console.log(''+mod_oper_running);
     if(!mod_oper_running){
-    var msg=new Object();
-    msg.sender=username;
-    msg.from=obj.from;
-    msg.to=obj.to;
-    if(obj.text.length==2){
-      msg.text='\n';
-    }else{
-    msg.text=obj.text[0];
-  }
-    msg.data='string';
-    sendText( JSON.stringify(msg));
-    showMsg( msg);
-    console.log(msg);
-  }
+      var msg=new Object();
+      msg.sender=username;
+      msg.from=obj.from;
+      msg.to=obj.to;
+      if(obj.text.length==2){
+        msg.text='\n';
+      }else{
+        msg.text=obj.text[0];
+      }
+      msg.data='string';
+      sendText( JSON.stringify(msg));
+      showMsg( msg);
+      console.log(msg);
+    }
+
     //console.log(obj.from);
-});
+  });
 
 
 
 
 
-    /*****************  Socket.io code *********************/
-    var username;
-    var socket = io.connect('http://127.0.0.1:3200');
+  /*****************  Socket.io code *********************/
+  var username;
+  var socket = io.connect('http://127.0.0.1:3200');
 
-socket.on('roomfull', function(room) {
-  console.log('this room is full cant connect ' + room);
+  socket.on('roomfull', function(room) {
+    console.log('this room is full cant connect ' + room);
   // updateRoomURL(ipaddr);
- 
+
 });
 
-socket.on('createdroom', function(room, clientId) {
-  console.log('Created room', room);
-  initiate = true;
-   socketIoMsg(true,"socket.io Connected");
+  socket.on('createdroom', function(room, clientId) {
+    console.log('Created room', room);
+    initiate = true;
+    socketIoMsg(true,"socket.io Connected");
  // grabWebCamVideo();
 
 });
 
-socket.on('joinedroom', function(room, clientId) {
-  console.log('This peer has joined room', room);
-  initiate = false;
-   socketIoMsg(true,"socket.io Connected");
-  createpeer_connection(initiate, config);
+  socket.on('joinedroom', function(room, clientId) {
+    console.log('This peer has joined room', room);
+    initiate = false;
+    socketIoMsg(true,"socket.io Connected");
+    createpeer_connection(initiate, config);
 //  grabWebCamVideo();
 });
 
 
 
-socket.on('ready', function() {
-  console.log('Socket is ready');
-  createpeer_connection(initiate, config);
-});
+  socket.on('ready', function() {
+    console.log('Socket is ready');
+    createpeer_connection(initiate, config);
+  });
 
-socket.on('log', function(array) {
-  console.log.apply(console, array);
-});
+  socket.on('log', function(array) {
+    console.log.apply(console, array);
+  });
 
-socket.on('message', function(message) {
-  console.log('Client received message:', message);
-  signalMsgCb(message);
-});
+  socket.on('message', function(message) {
+    console.log('Client received message:', message);
+    signalMsgCb(message);
+  });
 
 // Joining a room.
 // socket.emit('create or join', room);
@@ -95,13 +97,13 @@ socket.on('message', function(message) {
 // Leaving rooms and disconnecting from peers.
 socket.on('disconnect', function(reason) {
   console.log(`Disconnected: ${reason}.`);
-   socketIoMsg(false,"Connect");
+  socketIoMsg(false,"Connect");
 
 });
 
 socket.on('bye', function(room) {
   console.log(`Peer leaving room ${room}.`);
- 
+
 });
 /**
 * Send message to signaling server
@@ -111,7 +113,7 @@ function sendMessage(message) {
   socket.emit('message', message);
 }
 
-    /*******************************************************/
+/*******************************************************/
 
 
 /****************************************************************************
@@ -131,13 +133,13 @@ function signalMsgCb(message) {
   if (message.type === 'offer') {
     console.log('Got offer. Sending answer to peer.');
     peer_conn.setRemoteDescription(new RTCSessionDescription(message), function() {},
-                                  logError);
+      logError);
     peer_conn.createAnswer(onSessionLocal, logError);
 
   } else if (message.type === 'answer') {
     console.log('Got answer.');
     peer_conn.setRemoteDescription(new RTCSessionDescription(message), function() {},
-                                  logError);
+      logError);
 
   } else if (message.type === 'candidate') {
     peer_conn.addIceCandidate(new RTCIceCandidate({
@@ -149,7 +151,7 @@ function signalMsgCb(message) {
 
 function createpeer_connection(initiate, config) {
   console.log('Creating Peer connection as initiator?', initiate, 'config:',
-              config);
+    config);
   peer_conn = new RTCPeerConnection(config);
 
 // send any ice candidates to the other peer
@@ -195,15 +197,15 @@ function onDataChannelCreated(channel) {
   console.log('onDataChannelCreated:', channel);
 
   channel.onopen = function() {
-setWebRtcDispMsg("WebRTC Connected datachannel open")
+    setWebRtcDispMsg("WebRTC Connected datachannel open")
     console.log('CHANNEL opened!!!');
-   
+
   };
 
   channel.onclose = function () {
     setWebRtcDispMsg("WebRTC  datachannel closed")
     console.log('Channel closed.');
-   
+
   }
 
   channel.onmessage = (adapter.browserDetails.browser === 'firefox') ?
@@ -216,25 +218,11 @@ function receiveDataChromeFactory() {
   return function onmessage(event) {
     if (typeof event.data === 'string') {
       processMsg(event);
-    //   mod_oper_running=true;
-    //   var mod_to=new Object();
-    //   var data=JSON.parse(event.data);
-    //   mod_to=data.to;
-    //   if(data.text===''){
-    //     editor.replaceRange('',data.from,data.to);
-    //   }else{
-    // mod_to.ch=data.to.ch+1;
-    //   var txt=editor.getRange(data.from, mod_to);
-    //   if(txt!== data.text)
-    //  editor.replaceRange(data.text,data.from,data.to);
-    //  console.log('data: ' +  data);
-    //  }
-    //  mod_oper_running=false;
-    //   return;
-    }
+   
+  }
 
-    
-    
+
+
 
 };
 }
@@ -265,8 +253,8 @@ function showMsg (data) {
   console.log(data);
   if((data.sender)===(username))
    $("#msg_list").append("<li ><span class=\"badge badge-secondary\">"+data.sender+" </span> : "+data.text+"</li>");
-   else
-    $("#msg_list").append("<li ><span class=\"badge badge-primary\">"+data.sender+" </span> : "+data.text+"</li>");
+ else
+  $("#msg_list").append("<li ><span class=\"badge badge-primary\">"+data.sender+" </span> : "+data.text+"</li>");
 }
 
 
@@ -280,24 +268,24 @@ function logError(err) {
 }
 
 
- function processMsg (event) {
+function processMsg (event) {
   // body...
   mod_oper_running=true;
-      var mod_to=new Object();
-      var data=JSON.parse(event.data);
-      showMsg(data);
-      mod_to=data.to;
-      if(data.text===''){
-        editor.replaceRange('',data.from,data.to);
-      }else{
+  var mod_to=new Object();
+  var data=JSON.parse(event.data);
+  showMsg(data);
+  mod_to=data.to;
+  if(data.text===''){
+    editor.replaceRange('',data.from,data.to);
+  }else{
     mod_to.ch=data.to.ch+1;
-      var txt=editor.getRange(data.from, mod_to);
-      if(txt!== data.text)
+    var txt=editor.getRange(data.from, mod_to);
+    if(txt!== data.text)
      editor.replaceRange(data.text,data.from,data.to);
-     console.log('data: ' +  data);
-     }
-     mod_oper_running=false;
-     return;
+   console.log('data: ' +  data);
+ }
+ mod_oper_running=false;
+ return;
 } 
 
 
@@ -306,8 +294,8 @@ function setWebRtcDispMsg(msg){
 }
 function socketIoMsg(state,msg){
  connected_to_io=true;
-     $('#connect_comm').html(msg)
+ $('#connect_comm').html(msg)
 }
 
-	
+
 });
